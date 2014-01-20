10c290f5d3337adebaba858e48a2e79063b70987
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-01-20 10:43:20 -0400

    added reduction scripts

---
+++ reduction_scripts/makecube_Lima_C.py
@@ -0,0 +1,204 @@
+import astropy.io.fits as pyfits
+import itertools
+import sys
+sys.path.append('/Users/adam/repos/casaradio/branches/python/ginsburg/')
+import makecube
+import numpy as np
+np.seterr(all='ignore')
+
+scanrange=[6,21]
+ref1 = 6
+ref2 = 21
+refscans = [6,21]#,22,32]
+sourcename = "LimaBean"
+obsmode = "DecLatMap"
+mapname = 'LimaBean'
+outpath = '/Users/adam/observations/gbt/%smap/' % mapname
+
+filename = '/Users/adam/observations/gbt/AGBT12B_221_01/AGBT12B_221_01.raw.acs.fits'
+filepyfits = pyfits.open(filename,memmap=True)
+datapfits = filepyfits[1].data
+dataarr = datapfits.DATA
+
+for sampler in ['A10', 'A13', 'A14', 'A9', 'B17', 'B18', 'B21', 'B22', 'C25', 'C26',
+        'C29', 'C30', 'D33', 'D34', 'D37', 'D38']:
+
+    if sampler in ('A9','A13'):
+        off_template,off_template_in = makecube.make_off(filename, scanrange=[6,21],
+                exclude_velo=[-120,103,158,193], interp_vrange=[-600,600],
+                interp_polyorder=10, sampler=sampler, return_uninterp=True, 
+                percentile=50)
+    if sampler in ('C25','C29'): # h2 13CO
+        off_template = makecube.make_off(filename, scanrange=scanrange,
+                exclude_velo=[-10,70], interp_vrange=[-100,150],
+                interp_polyorder=10, sampler=sampler)
+    if sampler in ('D33','D37'): # H110a
+        off_template = makecube.make_off(filename, scanrange=scanrange,
+                exclude_velo=[-50,150], interp_vrange=[-150,250],
+                interp_polyorder=10, sampler=sampler)
+    else:
+        off_template = None
+
+    makecube.calibrate_cube_data(filename,
+            outpath+'12B_221_%ito%i_%s_F1.fits' %
+            (ref1,ref2,sampler),scanrange=scanrange,refscan1=ref1,refscan2=ref2,
+            feednum=1, refscans=refscans, sampler=sampler, filepyfits=filepyfits,
+            datapfits=datapfits, tau=0, dataarr=dataarr, obsmode=obsmode,
+            sourcename=sourcename, off_template=off_template)
+
+obsmode = 'RALongMap'
+scanrange=[22,32]
+ref1 = 22
+ref2 = 32
+refscans = [22,32]#,22,32]
+for sampler in ['A10', 'A13', 'A14', 'A9', 'B17', 'B18', 'B21', 'B22', 'C25', 'C26',
+        'C29', 'C30', 'D33', 'D34', 'D37', 'D38']:
+
+    if sampler in ('A9','A13'):
+        off_template = makecube.make_off(filename, scanrange=scanrange,
+                exclude_velo=[-125,103,158,193], interp_vrange=[-600,600],
+                interp_polyorder=10, sampler=sampler)
+    if sampler in ('C25','C29'): # h2 13CO
+        off_template = makecube.make_off(filename, scanrange=scanrange,
+                exclude_velo=[-10,70], interp_vrange=[-100,150],
+                interp_polyorder=10, sampler=sampler)
+    if sampler in ('D33','D37'): # H110a
+        off_template = makecube.make_off(filename, scanrange=scanrange,
+                exclude_velo=[-20,80], interp_vrange=[-100,150],
+                interp_polyorder=10, sampler=sampler)
+    else:
+        off_template = None
+
+    makecube.calibrate_cube_data(filename,
+            outpath+'12B_221_%ito%i_%s_F1.fits' %
+            (ref1,ref2,sampler),scanrange=scanrange,refscan1=ref1,refscan2=ref2,
+            feednum=1, refscans=refscans, sampler=sampler, filepyfits=filepyfits,
+            datapfits=datapfits, tau=0, dataarr=dataarr, obsmode=obsmode,
+            sourcename=sourcename, off_template=off_template)
+
+
+cubename='LimaBean_H2CO11_cube'
+# 15' x 12 ' 
+makecube.generate_header(0.256, 0.0220, naxis1=24, naxis2=24, pixsize=60,
+        naxis3=2400, cd3=1.0, clobber=True, restfreq=4.8296594e9)
+makecube.make_blank_images(cubename,clobber=True)
+
+files = ['/Users/adam/observations/gbt/LimaBeanmap/12B_221_6to21_A13_F1.fits',
+         '/Users/adam/observations/gbt/LimaBeanmap/12B_221_6to21_A9_F1.fits',
+         #'/Users/adam/observations/gbt/LimaBeanmap/12B_221_22to32_A13_F1.fits',
+         #'/Users/adam/observations/gbt/LimaBeanmap/12B_221_22to32_A9_F1.fits',
+         ]
+
+for fn in files:
+    makecube.add_file_to_cube(fn,
+        cubename+'.fits',nhits=cubename+'_nhits.fits',wcstype='V',
+        velocityrange=[-1200,1200],excludefitrange=[-225,250],
+        smoothto=2)
+
+import os
+os.system('./LimaBean_H2CO11_cube_starlink.sh')
+
+makecube.make_flats(cubename,vrange=[-20,60],noisevrange=[250,300])
+
+
+# SAMPLERS:
+# [('A10', 5500000000.0),
+#  ('A13', 4829659400.0),
+#  ('A14', 5500000000.0),
+#  ('A9', 4829659400.0),
+#  ('B17', 5200000000.0),
+#  ('B18', 5008992000.0),
+#  ('B21', 5200000000.0),
+#  ('B22', 5008992000.0),
+#  ('C25', 4593089000.0),
+#  ('C26', 4630000000.0),
+#  ('C29', 4593089000.0),
+#  ('C30', 4630000000.0),
+#  ('D33', 4874157000.0),
+#  ('D34', 4497776000.0),
+#  ('D37', 4874157000.0),
+#  ('D38', 4497776000.0)]
+#  OH F0-1 4.66024 
+#  OH F4-4 5.52344 
+#  OH F4-3 5.54704 
+
+
+for cubename,restfreq,samplers in (
+        ('LimaBean_H113a_cube', 4.497776e9, ('D34','D38')),
+        ('LimaBean_H110a_cube', 4.874157e9, ('D33','D37')),
+        ('LimaBean_H213CO_cube', 4.593089e9, ('C25','C29')),
+        ('LimaBean_H109a_cube', 5.008922e9, ('B18','B22')),
+        ('LimaBean_H112a_cube', 4.61879e9, ('C26','C30')),
+        ('LimaBean_OHF44_cube', 5.52344e9, ('A10','A14')),
+        ('LimaBean_CH3NH2_cube', 5.19543e9, ('B21','B17'))
+            ):
+
+
+    makecube.generate_header(0.256, 0.0220, naxis1=24, naxis2=24, pixsize=60,
+            naxis3=2400, cd3=1.0, clobber=True, restfreq=restfreq)
+    makecube.make_blank_images(cubename,clobber=True)
+
+    files = [
+             '/Users/adam/observations/gbt/LimaBeanmap/12B_221_6to21_%s_F1.fits' % samplers[0],
+             '/Users/adam/observations/gbt/LimaBeanmap/12B_221_6to21_%s_F1.fits' % samplers[1],]
+             #'/Users/adam/observations/gbt/LimaBeanmap/12B_221_22to32_%s_F1.fits' % samplers[0],
+             #'/Users/adam/observations/gbt/LimaBeanmap/12B_221_22to32_%s_F1.fits' % samplers[1],
+    for fn in files:
+        makecube.add_file_to_cube(fn,
+            cubename+'.fits',nhits=cubename+'_nhits.fits',wcstype='V',
+            velocityrange=[-1200,1200],excludefitrange=[-150,225])
+
+    os.system('chmod +x %s_starlink.sh' % cubename)
+    os.system('./%s_starlink.sh' % cubename)
+    makecube.make_flats(cubename,vrange=[-20,60],noisevrange=[250,300])
+
+
+
+import pyspeckit
+#pyspeckit.wrappers.cube_fit('LimaBean_H2CO11_cube_smooth.fits','LimaBean_H2CO11_parfits.fits','LimaBean_H2CO11_noise.fits',fittype='formaldehyde',absorption=True,signal_cut=2)
+
+
+from pylab import *
+def some_plots(xx,yy,dolegend=False,dohalpha=False):
+    c13 = h213cocube.get_spectrum(xx,yy)
+    c12 = cube.get_spectrum(xx,yy)
+    c12.baseline(exclude=[-225,200],order=5)
+    c13.baseline(exclude=[-225,200],order=5)
+    c13.plotter(label='H$_{2}$$^{13}$CO',axis=gca())
+    c12.plotter(axis=c13.plotter.axis,clear=False,color='b',label="H$_{2}$CO")
+    (c13*6).plotter(label='6$\\times$H$_{2}$$^{13}$CO',axis=gca(),color='r',clear=False)
+    if dolegend:
+        legend(loc='best')
+    if dohalpha:
+        halpha = h110acube.get_spectrum(xx,yy)
+        halpha.baseline(exclude=[-225,200],order=5)
+        (halpha*5).plotter(axis=c13.plotter.axis,clear=False,color='darkgreen')
+    gca().set_xlim(-100,100)
+    draw()
+        
+
+# some plots
+if not 'do_some_plots' in locals():
+    do_some_plots=False
+    dohalpha=True
+if do_some_plots:
+    subplots_adjust(wspace=0,hspace=0,left=0.05,right=0.95,bottom=0.05,top=0.95)
+    cube = pyspeckit.Cube('LimaBean_H2CO11_cube_sub.fits')
+    h213cocube = pyspeckit.Cube('LimaBean_H213CO_cube_sub.fits')
+    h110acube = pyspeckit.Cube('LimaBean_H110a_cube_sub.fits')
+
+    figure(1); clf()
+    nx,ny = 5,5
+    for ii,(spy,spx) in enumerate(itertools.product(range(nx),range(ny))):
+        sp = subplot(nx,ny,ii+1)
+        sp.zorder = nx-spx+ny*spy
+        some_plots(spx+9,spy+9,dolegend=(ii==24),dohalpha=True)
+        if dohalpha:
+            sp.set_ylim(-2.5,1)
+        else:
+            sp.set_ylim(-2.5,0.5)
+        if spx > 0: 
+            sp.set_yticks([])
+        if spy < ny-1: 
+            sp.set_xticks([])
+        
