463508226195d3df55e6d8864d9b6288b4d6082c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-22 17:37:23 -0400

    shrink map so it fits in the alotted time

--- Ka_LimaBean_SmallMap_Lat.py
+++ Ka_LimaBean_SmallMap_Lat.py
@@ -1,5 +1,5 @@
 """
-Create a 12x15' map of the Lima Bean at Ka band
+Create a 10x15' map of the Lima Bean at Ka band
 
 *This observation will take 1.92 hours on-source, *PLUS* overheads, so about 2.2 hours*
 
@@ -22,23 +22,23 @@ Slew("LimaBean")
 Balance()
 
 amintodeg = 1/60.
+arcsectodeg = 1/3600.
 # samplerate = 30/minute = 0.5/s
 # beam ~ 25"
 # 4 samples/beam
 # 8 seconds / arcminute
-# or: 27" beam, 2.5 samples per peam, 1.2 s per sample:
-# 27/2.5/1.2 = 9
-# -> 10.8"/sample
 scanrate = 9 # arcmin/min
-scanheight = 12 # arcmin
-scanwidth = 15 # arcmin
+scanheight = 8 # arcmin
+scanwidth = 8 # arcmin
+hdelta = 10.0 * arcsectodeg
 # nscans = 6 * 12 = 72
 
 # vertical scans
 DecLatMap('LimaBean',     #center of map
     hLength = Offset("Galactic",scanwidth*amintodeg,0.0,cosv=True),
     vLength = Offset("Galactic",0.0,scanheight*amintodeg,cosv=True),
-    hDelta  = Offset("Galactic",(1./6.)*amintodeg,0.0,cosv=True),
+    hDelta  = Offset("Galactic",hdelta,0.0,cosv=True),
     scanDuration = scanheight/scanrate * 60,
     beamName="1")
+
 
